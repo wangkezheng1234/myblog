97ba2504d44273afeb626153f40751c27eaf553d
wankezheng <wangkezheng12342163.com>
2016-02-29 21:08:05 +0800

    时间显示风格的调整

--- usr/themes/WhiteTheme/index.php
+++ usr/themes/WhiteTheme/index.php
@@ -15,7 +15,7 @@
 	<?php while($this->next()): ?>
       <div class="cont">
         <h2><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-        <p class="entry_data">发布时间：<?php $this->date('Y-m-d H:i:s'); ?> &nbsp;分类：<?php $this->category(','); ?> &nbsp; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('No Comments', '1 Comment', '%d 评论'); ?></a></p>
+        <p class="entry_data">发布时间：<?php $this->date('Y-m-d H:i:s'); ?> &nbsp;分类：<?php $this->category(','); ?> &nbsp; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('0 评论', '1 评论', '%d 评论'); ?></a></p>
         <div class="content">
           <?php $this->content('Read More →'); ?>
         </div>
