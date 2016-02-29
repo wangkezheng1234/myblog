7e49230a39b526fb20fc134a853e2b6f3acefafb
wankezheng <wangkezheng12342163.com>
2016-02-29 22:15:33 +0800

    时间显示风格的调整

--- usr/themes/WhiteTheme/page.php
+++ usr/themes/WhiteTheme/page.php
@@ -3,7 +3,7 @@
   <div class="left">
     <div class="cont">
       <h2><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-      <p class="entry_data">发布时间：<?php $this->date('F j, Y'); ?> // <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('No Comments', '1 Comment', '%d Comments'); ?></a></p>
+      <p class="entry_data">发布时间：<?php $this->date('Y-m-d H:i:s'); ?> &nbsp; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('0 评论', '1 评论', '%d 评论'); ?></a></p>
       <div class="content">
         <?php $this->content(); ?>
       </div>
